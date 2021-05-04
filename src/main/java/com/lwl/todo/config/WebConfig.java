31a3345225e7480a675d8de9930177bb1204b7c3
avulalakshman <learnwithlakshman@gmail.com>
2021-05-05 01:53:24 +0530

    Changed headers

--- src/main/java/com/lwl/todo/config/WebConfig.java
+++ src/main/java/com/lwl/todo/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS").allowedHeaders("*");
 	}
 
 	@Bean
