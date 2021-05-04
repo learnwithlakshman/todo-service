005b7dad28f12a5495ca134745d0120a190c1c05
avulalakshman <lakshman.avula@outlook.com>
2021-05-05 01:35:12 +0530

    Update WebConfig.java
    Added cors header types

--- src/main/java/com/lwl/todo/config/WebConfig.java
+++ src/main/java/com/lwl/todo/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders = "*";
 	}
 
 	@Bean
