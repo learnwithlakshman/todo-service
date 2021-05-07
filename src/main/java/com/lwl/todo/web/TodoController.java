56a2f706381512ce029cf00b5ce47c9a53230dee
avulalakshman <learnwithlakshman@gmail.com>
2021-05-07 07:13:38 +0530

    Changed request param to path variable in delete method

--- src/main/java/com/lwl/todo/web/TodoController.java
+++ src/main/java/com/lwl/todo/web/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -70,8 +71,8 @@ public class TodoController {
 	
 	
 
-	@DeleteMapping("{id}")
-	public ResponseEntity<HttpStatus> deleteTodo(@RequestParam("id") String id) {
+	@DeleteMapping("/{id}")
+	public ResponseEntity<HttpStatus> deleteTodo(@PathVariable("id") String id) {
 		try {
 			boolean isDeleted = todoService.deleteTodo(id);
 			if (isDeleted)
